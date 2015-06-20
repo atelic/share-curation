a096b842349536b0538ab8e9015034b3f63a42dc
Eric Barbour <emb4gu@virginia.edu>
2015-06-20 10:10:13 -0400

    / and /admin routes

---
+++ crowd/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class UserName(models.Model):
+  pass  
+
+class Password(models.Model):
+  pass  
--- shareCuration/urls.py
+++ shareCuration/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from crowd import views
+
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'shareCuration.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.index, name='index'),
+    # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^crowd/', include(admin.site.urls)),
 ]
