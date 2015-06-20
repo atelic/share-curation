8105d97bbe9aa43de72070fe4c931109c570cd94
Eric Barbour <emb4gu@virginia.edu>
2015-06-20 17:16:19 -0400

    Sending filtered SHARE payload now

--- crowd/views.py
+++ crowd/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from sharepa import ShareSearch
-import json
 from django.http import HttpResponse, JsonResponse
+import json
+
+from sharepa import ShareSearch, bucket_to_dataframe
+from elasticsearch_dsl import F, Q
+
 
 # Create your views here.
 def index(request):
@@ -11,7 +14,16 @@ def homepage(request):
     return render(request, 'homepage.html')
 
 def ten(request):
-    search = ShareSearch()
-    first_ten = search.execute()
-    return JsonResponse(first_ten.to_dict())
- 
+    # abstracts, doi, publisher, uri, languages, sponsorships, licenses
+    from elasticsearch_dsl import F, Q
+
+    my_search = ShareSearch()
+    my_search = my_search.query(
+    'query_string', # Type of query, will accept a lucene query string
+    query='NOT description:* OR NOT uri:* OR NOT publisher:* OR NOT doi:* OR NOT sponsorships:* OR NOT licenses:* OR NOT languages:*',
+    analyze_wildcard=True  # This will make elasticsearch pay attention to the asterisk (which matches anything)
+    )
+
+    my_results = my_search.execute()
+    return JsonResponse(my_results.to_dict())
+
