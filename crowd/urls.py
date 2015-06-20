f8fc0e8423829b16784b2eea356eda425d8d2086
Lauren Barker <leb2dg@virginia.edu>
2015-06-20 15:53:22 -0400

    working homepage

--- crowd/urls.py
+++ crowd/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from shareCuration import settings
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^homepage/$', views.homepage, name='homepage'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
